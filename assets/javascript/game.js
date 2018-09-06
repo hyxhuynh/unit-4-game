5fafb3a580a9c0ef2682799281dfbd32c96f760d
Hy Huynh <hyxhuynh@gmail.com>
2018-09-06 01:48:00 -0700

    modified game.js and added minor changes to index.html and style.css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,11 +9,18 @@ $(document).ready(function() {
         var wins = 0;
         var losses = 0;
 
-        //Returns a random integer between min (inclusive) and max (inclusive)
+        // Target Score
+        $("#numberRandom").text(targetScore);
+
+        // FUNCTIONS
+        // Returns a random integer between min (inclusive) and max (inclusive)
         function getRandomInt(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
         }
         
+
+        // This function is used later when clicking on the images. Depending on which image clicked, random score from that image will add onto the player's score. 
+        // Depending on the player's score, it will display either "You won" or "You lost"
         function processClick(value) {
             playerScore += value;
             $("#totalScore").text(playerScore);
@@ -30,16 +37,7 @@ $(document).ready(function() {
             };
         }
         
-        // Target Score
-        $("#numberRandom").text(targetScore);
-
-        updateField()
-        // Click the image and add score to the user's score
-        $("#blue").on("click", () => processClick(crystalBlue));
-        $("#green").on("click", () => processClick(crystalGreen));
-        $("#yellow").on("click", () => processClick(crystalYellow));
-        $("#red").on("click", () => processClick(crystalRed));
-    
+        // Reset function to refresh random numbers for crystals and target score, and player's score
         function reset() {
             crystalBlue = getRandomInt (1, 12);
             crystalGreen = getRandomInt (1, 12);
@@ -52,21 +50,20 @@ $(document).ready(function() {
             
         }
 
+        // Clear "You won" or "You lost" text for a new game
         function updateField () {
-            $(".textWinLost").text("");
-            
+            $(".textWinLost").text("");   
         }
-    
-    //Using jQuery to add the numbers to the images and the scores to the appropriate div
-    // .val grabs value off elements or add values; add var crystal 1-4 to different images
-    // click handler
-    // add to player's score
-    // each crystal has the same class "crystals" for eg.
-    // when user click run the event handler
-    // $(this).val() as .val is a method, no parametor: pull values from 'this'
-    // after adding to player score, if the score match the target score
-    // if match display message
-    // check if the player score is higher than the target score, increment losses
-    // if neither contidions are met, continue playing 
+
+        // EVENT LISTENERS
+
+        // Clear "You won" or "You lost" text for a new game
+        updateField()
+        // Click the image and add score to the user's score
+        $("#blue").on("click", () => processClick(crystalBlue));
+        $("#green").on("click", () => processClick(crystalGreen));
+        $("#yellow").on("click", () => processClick(crystalYellow));
+        $("#red").on("click", () => processClick(crystalRed));
+
 })
 
